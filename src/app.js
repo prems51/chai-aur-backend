de98a81740048fa13ecac6436537cb6d1cb469cd
prems51 <prem.s244001@gmail.com>
2025-01-30 22:52:55 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_OPTION,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export default app;
